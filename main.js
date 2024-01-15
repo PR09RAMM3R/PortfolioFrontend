199528e3ae3b58b28a37e1d6a9346aff8e5029dc
Amir Asadulloh <111172391+PR09RAMM3R@users.noreply.github.com>
2024-01-15 19:33:50 +0500

    bugs fixed and add animations

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-let menu = document.querySelector('.hamburgerMenu');
-let menuExit = document.querySelector('.exitMenu');
-let menuOpen = document.querySelector('.hambergurMenuActive');
+// Burger menu codes
+
+let menu = document.querySelector('.hamburgerMenu'),
+    menuExit = document.querySelector('.exitMenu'),
+    menuOpen = document.querySelector('.hambergurMenuActive');
 menu.addEventListener('click', function() {
     menuOpen.classList.add('active')
 });
 menuExit.addEventListener('click', function() {
     menuOpen.classList.remove('active')
+})
+
+
+// Head Title codes
+
+let docTitle = document.title;
+window.addEventListener('blur', () => {
+    document.title = 'Come Back :(';
+})
+window.addEventListener('focus', () => {
+    document.title = docTitle;
 })
